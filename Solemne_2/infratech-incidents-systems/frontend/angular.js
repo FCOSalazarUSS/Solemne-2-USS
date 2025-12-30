d4c9dc31a2eb7b21f2fef6d7ac57ee54afda8a2c
FCOSalazarUSS <fsalazarc5@correo.uss.cl>
2025-12-30 03:01:08 -0300

    Delete Solemne_2/infratech-incidents-systems directory

--- Solemne_2/infratech-incidents-systems/frontend/angular.js
+++
@@ -1,7 +0,0 @@
-this.form = this.fb.group({
-  tipo: ['', Validators.required],
-  area: ['', Validators.required],
-  descripcion: ['', Validators.required],
-  prioridad: ['', Validators.required],
-  tecnico: ['', Validators.required]
-});
